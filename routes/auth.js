21f16fcb7198c84502ad4d48de58f9aed35d9a80
amarmanhala <amar.manhala@gmail.com>
2022-12-31 09:54:39 -0500

    minor

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,31 @@
-const config = require('config');
+const config = require("config");
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const userSchema = require('../models/users');
-const isAuthValid = require('../validations/auth')
-const _ = require('lodash');
-const bcrypt = require('bcrypt');
-
-
+const userSchema = require("../models/users");
+const isAuthValid = require("../validations/auth");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
 
 //User Model
 const User = mongoose.model("User", userSchema);
 
 //Add new user
-router.post('/', async (req, res) => {
-
-  //Check isUser valid or not with Joi 
+router.post("/", async (req, res) => {
+  //Check isUser valid or not with Joi
   const response = await isAuthValid(req.body);
-  if (response.error) return res.status(400).send(response.error.details[0].message);
+  if (response.error)
+    return res.status(400).send(response.error.details[0].message);
 
-let user = await User.findOne( { email: req.body.email })
-if (!user) return res.status(400).send("Invalid email or password");
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
-  
+
   const token = user.generateAuthToken();
-  
+
   res.send(token);
-
-
-})
-
+});
 
 module.exports = router;
